a49dc7fe8d1876f9b117f9e755266978850e4306
Bill G <gbilly3@gmail.com>
2019-03-08 21:24:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,12 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   
- pigeion_list do 
+ pigeion_list do |key, value|
+   value.each do |name|
    
    
    
-   
+  name 
+  hash 
    
 end
