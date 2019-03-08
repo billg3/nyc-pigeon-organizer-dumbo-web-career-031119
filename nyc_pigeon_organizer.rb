f0e7f8617639a774a6a7736e2f589e06af1d207f
Bill G <gbilly3@gmail.com>
2019-03-08 22:25:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ def nyc_pigeon_organizer(data)
  data.each do |key, value|
    value.each do |attribute, names|
    names.each do |x|
-  #binding.pry
+   binding.pry
     
     pigeons[x] = {key => [attribute.to_s]}
    
